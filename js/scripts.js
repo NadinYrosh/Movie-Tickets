ad6409f4a6b786f439813548aa2d4e2089a53f10
Nadiya Yeroshkina and Patrick Wells <nadiya@>
2016-04-05 15:21:46 -0700

    add js functionality for early beard and regular price tickets

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,43 @@
+var timesFunction = function() {
+
+  var regularPrice = [];
+
+  for (j = 10; j < 23; j++) {
+    regularPrice.push(j);
+  }
+  return regularPrice;
+};
+
+
+
+
+
+
 $(document).ready(function(){
 
+ $("form").submit(function(event){
+   event.preventDefault();
+
+   $(".earlyPrice").empty();
+   $(".regularPrice").empty();
+
+   var date = $("select").val();
+
+   var timePrices = timesFunction();
+
+   console.log(timePrices);
+
+   for (j = 0; j < timePrices.length; j++) {
+     if(timePrices[j] <= 13) {
+       $(".earlyPrice").append("<label class='radio-inline'><input type='radio' name='inlineRadioOptions' id='inlineRadio1' value='option1'>" + timePrices[j] + ":45</label>");
+     }
+     if(timePrices[j] > 13) {
+       $(".regularPrice").append("<label class='radio-inline'><input type='radio' name='inlineRadioOptions' id='inlineRadio1' value='option1'>" + timePrices[j] + ":45</label>");
+     }
+   }
+
+   $("#times").show();
+
+  });
 
 });
